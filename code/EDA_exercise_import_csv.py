2b0b62b2ac38afd07f635264c7490067dbea30e8
fernandosansegundo <fernandosansegundo@gmail.com>
2022-08-25 12:49:05 +0200

    WIP resume Session02 EDA

---
+++ code/EDA_exercise_import_csv.py
@@ -0,0 +1,5 @@
+framingham_url = "https://raw.githubusercontent.com/mbdfmad/fmad2122/main/data/framingham.csv"
+framingham = pd.read_csv(framingham_url)
+
+flights_url = "https://github.com/byuidatascience/data4python4ds/raw/master/data-raw/flights/flights.csv"
+flights = pd.read_csv(flights_url)
