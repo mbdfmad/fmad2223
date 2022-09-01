b2d9c635116f19bdd90fa88b4afd47e6705c8391
fernandosansegundo <fernandosansegundo@gmail.com>
2022-09-01 15:21:42 +0200

    Fixing typos in first session notes

--- code/EDA_exercise_import_csv.py
+++
@@ -1,5 +0,0 @@
-framingham_url = "https://raw.githubusercontent.com/mbdfmad/fmad2122/main/data/framingham.csv"
-framingham = pd.read_csv(framingham_url)
-
-flights_url = "https://github.com/byuidatascience/data4python4ds/raw/master/data-raw/flights/flights.csv"
-flights = pd.read_csv(flights_url)
